0b07da60c9a07a45ed3f3a8ca15e23bd15fc1134
karupoimou <karupoimou@gmail.com>
2019-04-22 03:20:38 +0900

    1.0

---
+++ js/popup.js
@@ -0,0 +1,19 @@
+window.onload = function ()
+{
+  //Run when opened
+}
+
+//Aboutボタン（常に有効）
+$(function(){
+　$("#btn").click(function(){
+
+  //Executed when the button is pressed
+
+  console.log('Pushed!');//For debugging
+
+　});
+});
+
+
+//debugging url (change "mocolllbmbndodjhaiochlcgpdoabdom" to your chrome Extension ID
+//chrome-extension://mocolllbmbndodjhaiochlcgpdoabdom/popup.html
